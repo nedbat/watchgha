637d23df3e8a4c9713fa160fa0a87a442c56c47b
Ned Batchelder <ned@nedbatchelder.com>
2023-03-07 05:20:33 -0500

    more information shown

--- src/jpretty/watch_runs.py
+++ src/jpretty/watch_runs.py
@@ -1,5 +1,7 @@
 import datetime
 import itertools
+import json
+import os
 
 import requests
 from rich.console import Console
@@ -9,9 +11,9 @@ from .utils import nice_time, DictAttr
 
 
 REPO = "nedbat/coveragepy"
-BRANCH = "master"
+BRANCH = "nedbat/test"
 
-URL = f"https://api.github.com/repos/{REPO}/actions/runs?per_page=50&branch={BRANCH}"
+URL = f"https://api.github.com/repos/{REPO}/actions/runs?per_page=20&branch={BRANCH}"
 
 bucketer = DatetimeBucketer(5)
 console = Console(highlight=False)
@@ -31,19 +33,37 @@ def run_sort_key(run_data):
         run_data["name"],
     )
 
+SAVE_JSON = int(os.environ.get("SAVE_JSON", "0"))
+
+class Http:
+    def __init__(self):
+        self.count = itertools.count()
+
+    def get_json(self, url):
+        resp = requests.get(url)
+        data = resp.json()
+        if SAVE_JSON:
+            with open(f"get_{next(self.count):03d}.json", "w") as jout:
+                json.dump(data, jout, indent=4)
+        return data
+
+http = Http()
+
 def main():
-    runs = requests.get(URL).json()
+    runs = http.get_json(URL)
     runs = runs["workflow_runs"]
 
     for run in runs:
         run["started_dt"] = datetime.datetime.fromisoformat(run["run_started_at"])
 
     runs.sort(key=run_sort_key, reverse=True)
+    run_names_seen = set()
 
-    for i, (k, g) in enumerate(itertools.groupby(runs, key=run_group_key)):
+    for _, g in itertools.groupby(runs, key=run_group_key):
         these_runs = list(g)
-        if i == 1:
-            console.print()
+        these_runs_names = set(r["name"] for r in these_runs)
+        if not (these_runs_names - run_names_seen):
+            continue
         _ = DictAttr(these_runs[0])
         console.print(
             f"[white bold]{_.display_title}[/] " +
@@ -60,10 +80,35 @@ def main():
             console.print(
                 f"   " +
                 f"{_.status:12} " +
-                f"[{cstyles.get(_.conclusion, 'default')}]{_.conclusion:10}[/] " +
+                f"[{cstyles.get(_.conclusion, 'default')}]{_.conclusion or '':10}[/] " +
                 f"{_.name:20} " +
-                f"  [blue link={_.url}]view {_.url.split('/')[-1]}[/]"
+                f"  [blue link={_.html_url}]view {_.url.split('/')[-1]}[/]"
             )
+
+            jobs = http.get_json(r["jobs_url"])["jobs"]
+            for j in jobs:
+                current_step = "---"
+                style = "default"
+                if j["status"] == "queued":
+                    current_step = "queued"
+                else:
+                    steps = j["steps"]
+                    for i, step in enumerate(steps):
+                        if step["status"] == "completed" and step["conclusion"] == "failure":
+                            current_step = f"{i+1}/{len(steps)}: {step['name']}"
+                            style = cstyles["failure"]
+                            break
+                        if step["status"] == "in_progress":
+                            current_step = f"{i+1}/{len(steps)}: {step['name']}"
+                            break
+                    else:
+                        current_step = steps[-1]["name"]
+                        style = cstyles["success"]
+
+                _ = DictAttr(j)
+                console.print(f"      {_.name:30} [{style}]{current_step}[/]")
+
+        run_names_seen.update(these_runs_names)
 
 
 if __name__ == "__main__":
