5c9f4d67617f3d178290c5f1c97bbd3eba152412
Ned Batchelder <ned@nedbatchelder.com>
2025-03-26 07:06:25 -0400

    test: use the sample data as a test

---
+++ src/watchgha/sample_data.py
@@ -0,0 +1,153 @@
+import datetime
+import itertools
+import json
+
+from .data_core import FINISHED, draw_runs
+
+next_id = itertools.count().__next__
+
+NOW = datetime.datetime.fromisoformat("2025-03-26T16:55:59")
+
+def next_isodatetime():
+    # The sample data makes ~16 datetimes.  We want them all to be within 5
+    # seconds, but not all within the same second.
+    when = NOW - datetime.timedelta(milliseconds=next_id() * 100)
+    return when.isoformat(timespec='seconds') + "Z"
+
+def run_common():
+    return {
+        "id": next_id(),
+        "display_title": "fix: most awesome fix",
+        "head_branch": "nedbat/test",
+        "html_url": "https://github.com/owner/repo/actions/runs/123456789",
+        "event": "push",
+        "head_sha": "4b2ff58124791953563fdb52e40d9ab79d274d9a",
+        "run_started_at": next_isodatetime(),
+    }
+
+def job_common():
+    return {
+        "id": next_id(),
+        "created_at": next_isodatetime(),
+    }
+
+SAMPLE_DATA = {
+    "demo:one": {
+        "workflow_runs": [
+            {
+                **run_common(),
+                "name": "Test suite",
+                "status": "in_progress",
+                "jobs_url": "demo:jobs_tests",
+            },
+            *[
+                {
+                    **run_common(),
+                    "name": f"A {conc} run",
+                    "status": "completed",
+                    "conclusion": conc,
+                    "jobs_url": "demo:jobs_1",
+                }
+                for conc in FINISHED
+            ],
+            {
+                **run_common(),
+                "name": "Malicious \n\n\n\n data",
+                "status": "in_progress",
+                "jobs_url": "demo:jobs_malicious",
+            },
+        ],
+    },
+    "demo:jobs_1": {
+        "jobs": [
+            {
+                **job_common(),
+                "name": "Just one job",
+                "status": "queued",
+            },
+        ],
+    },
+    "demo:jobs_tests": {
+        "jobs": [
+            {
+                **job_common(),
+                "name": "A delayed job",
+                "status": "queued",
+            },
+            {
+                **job_common(),
+                "name": "A finished job",
+                "status": "completed",
+                "conclusion": "success",
+            },
+            {
+                **job_common(),
+                "name": "A failed job",
+                "status": "completed",
+                "conclusion": "failure",
+                "steps": [
+                    {
+                        "status": "completed",
+                        "conclusion": "success",
+                    },
+                    {
+                        "status": "completed",
+                        "conclusion": "failure",
+                        "name": "Didn't work",
+                    },
+                    {
+                        "status": "queued",
+                    },
+                ],
+            },
+            *[
+                {
+                    **job_common(),
+                    "name": f"Test suite Py 3.{py}",
+                    "status": "in_progress",
+                    "steps": [
+                        *[
+                            {
+                                "status": "completed",
+                                "conclusion": "success",
+                            }
+                            for _ in range(3 - py % 2)
+                        ],
+                        {
+                            "status": "skipped",
+                        },
+                        {
+                            "name": "Prep tests" if (py % 2) else "Run the tests",
+                            "status": "in_progress",
+                        },
+                        *[
+                            {
+                                "status": "queued",
+                            }
+                            for _ in range(2 + py % 2)
+                        ],
+                    ],
+                }
+                for py in [8, 9, 10, 11]
+            ],
+        ],
+    },
+    "demo:jobs_malicious": {
+        "jobs": [
+            {
+                **job_common(),
+                "name": "A \x1B[1mMALICIOUS\x1B[0m job",
+                "status": "queued",
+            },
+        ],
+    },
+}
+
+
+async def sample_datafn(url):
+    url = url.partition("?")[0]
+    return json.dumps(SAMPLE_DATA[url])
+
+
+def sample(outfn):
+    draw_runs(["demo:one"], datafn=sample_datafn, outfn=outfn)
---
+++ tests/test_sample_data.py
@@ -0,0 +1,33 @@
+import io
+
+from textwrap import dedent
+
+from watchgha.sample_data import sample
+
+
+def test_sample_data():
+        stream = io.StringIO()
+        sample(outfn=lambda s: print(s, file=stream))
+
+        expected = dedent("""\
+            [white bold]fix: most awesome fix[/] nedbat/test \\[push]   [dim]4b2ff5812479  @12:55pm[/]
+               [default]↻ in_progress [/] [white bold]Test suite      [/]   [blue link=https://github.com/owner/repo/actions/runs/123456789]view 123456789[/]
+                  A delayed job                  [default]⏲[/] [default]queued[/]
+                  A failed job                   [red bold]✗[/] [red bold]failure Didn't work[/]
+                  A finished job                 [green bold]✓[/] [green bold]success[/]
+                  Test suite Py 3.8              [default]↻[/] [green]•[/][green]•[/][green]•[/][default]◦[/][white]•[/][dim white]•[/][dim white]•[/][default] Run the tests[/]
+                  Test suite Py 3.9              [default]↻[/] [green]•[/][green]•[/][default]◦[/][white]•[/][dim white]•[/][dim white]•[/][dim white]•[/][default] Prep tests[/]
+                  Test suite Py 3.10             [default]↻[/] [green]•[/][green]•[/][green]•[/][default]◦[/][white]•[/][dim white]•[/][dim white]•[/][default] Run the tests[/]
+                  Test suite Py 3.11             [default]↻[/] [green]•[/][green]•[/][default]◦[/][white]•[/][dim white]•[/][dim white]•[/][dim white]•[/][default] Prep tests[/]
+               [default]↻ in_progress [/] [white bold]Malicious  data[/]   [blue link=https://github.com/owner/repo/actions/runs/123456789]view 123456789[/]
+                  A [1mMALICIOUS[0m job        [default]⏲[/] [default]queued[/]
+               [green bold]✓ success     [/] [white bold]A success run   [/]   [blue link=https://github.com/owner/repo/actions/runs/123456789]view 123456789[/]
+               [red bold]✗ startup_failure[/] [white bold]A startup_failure run[/]   [blue link=https://github.com/owner/repo/actions/runs/123456789]view 123456789[/]
+                  Just one job                   [default]⏲[/] [default]queued[/]
+               [default]⏭ skipped     [/] [white bold]A skipped run   [/]   [blue link=https://github.com/owner/repo/actions/runs/123456789]view 123456789[/]
+               [red bold]✗ failure     [/] [white bold]A failure run   [/]   [blue link=https://github.com/owner/repo/actions/runs/123456789]view 123456789[/]
+                  Just one job                   [default]⏲[/] [default]queued[/]
+               [default]† cancelled   [/] [white bold]A cancelled run [/]   [blue link=https://github.com/owner/repo/actions/runs/123456789]view 123456789[/]
+                  Just one job                   [default]⏲[/] [default]queued[/]
+            """)
+        assert stream.getvalue() == expected
